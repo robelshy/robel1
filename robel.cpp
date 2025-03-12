31c49a89bccedfd0aa27b1009a89f390322fe048
robelshy <robashife25@gmail.com>
2025-03-12 16:55:10 +0300

    day 3

---
+++ robel.cpp
@@ -0,0 +1,7 @@
+#include <iostream>  // Include the input-output stream library
+
+int main() {
+    // Print "Hello, World!" to the console
+    std::cout << "Hello, World!" << std::endl;  
+    return 0;  // Return 0 to indicate the program ended successfully
+}
